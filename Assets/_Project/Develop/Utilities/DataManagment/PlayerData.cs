1518e0db765960397a0c22d77ea9d8cd09e1ef3e
Olga Salakhova <Olyushon@users.noreply.github.com>
2025-07-10 18:07:05 +0300

    PlayerData

---
+++ Assets/_Project/Develop/Utilities/DataManagment/PlayerData.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using Meta.Features.Wallet;
+
+namespace Utilities.DataManagment
+{
+    public class PlayerData
+    {
+        public Dictionary<CurrencyType, int> WalletData;
+    }
+}
