2ba4a8f4bae35ae0696a2a9166506386a9cb0ad7
Olga Salakhova <Olyushon@users.noreply.github.com>
2025-08-20 18:14:05 +0500

    Генерация файла с вспомогательными методами

---
+++ Assets/_Project/Develop/Gameplay/EntitiesCore/Generated/EntityAPI.cs
@@ -0,0 +1,43 @@
+namespace Gameplay.EntitiesCore
+{
+	public partial class Entity
+	{
+		public Gameplay.Features.MovementFeature.MoveDirection MoveDirectionC => GetComponent<Gameplay.Features.MovementFeature.MoveDirection>();
+
+		public Utilities.Reactive.ReactiveVariable<UnityEngine.Vector3> MoveDirection => MoveDirectionC.Value;
+
+		public Gameplay.EntitiesCore.Entity AddMoveDirection()
+		{
+			return AddComponent(new Gameplay.Features.MovementFeature.MoveDirection() { Value = new Utilities.Reactive.ReactiveVariable<UnityEngine.Vector3>() }); 
+		}
+
+		public Gameplay.EntitiesCore.Entity AddMoveDirection(Utilities.Reactive.ReactiveVariable<UnityEngine.Vector3> value)
+		{
+			return AddComponent(new Gameplay.Features.MovementFeature.MoveDirection() {Value = value}); 
+		}
+
+		public Gameplay.Features.MovementFeature.MoveSpeed MoveSpeedC => GetComponent<Gameplay.Features.MovementFeature.MoveSpeed>();
+
+		public Utilities.Reactive.ReactiveVariable<System.Single> MoveSpeed => MoveSpeedC.Value;
+
+		public Gameplay.EntitiesCore.Entity AddMoveSpeed()
+		{
+			return AddComponent(new Gameplay.Features.MovementFeature.MoveSpeed() { Value = new Utilities.Reactive.ReactiveVariable<System.Single>() }); 
+		}
+
+		public Gameplay.EntitiesCore.Entity AddMoveSpeed(Utilities.Reactive.ReactiveVariable<System.Single> value)
+		{
+			return AddComponent(new Gameplay.Features.MovementFeature.MoveSpeed() {Value = value}); 
+		}
+
+		public Gameplay.Common.RigidbodyComponent RigidbodyC => GetComponent<Gameplay.Common.RigidbodyComponent>();
+
+		public UnityEngine.Rigidbody Rigidbody => RigidbodyC.Value;
+
+		public Gameplay.EntitiesCore.Entity AddRigidbody(UnityEngine.Rigidbody value)
+		{
+			return AddComponent(new Gameplay.Common.RigidbodyComponent() {Value = value}); 
+		}
+
+	}
+}
--- Assets/_Project/Develop/Gameplay/EntitiesCore/TestPartialEntity.cs
+++ Assets/_Project/Develop/Gameplay/EntitiesCore/TestPartialEntity.cs
@@ -7,22 +7,22 @@ namespace Gameplay.EntitiesCore
 {
     public partial class Entity 
     {
-        public RigidbodyComponent RigidbodyC => GetComponent<RigidbodyComponent>();
-        public Rigidbody Rigidbody => RigidbodyC.Value;
-        public Entity AddRigidbody(Rigidbody rigidbody) => AddComponent(new RigidbodyComponent(){Value = rigidbody});
+        // public RigidbodyComponent RigidbodyC => GetComponent<RigidbodyComponent>();
+        // public Rigidbody Rigidbody => RigidbodyC.Value;
+        // public Entity AddRigidbody(Rigidbody rigidbody) => AddComponent(new RigidbodyComponent(){Value = rigidbody});
 
-        public MoveDirection MoveDirectionC => GetComponent<MoveDirection>();
-        public ReactiveVariable<Vector3> MoveDirection => MoveDirectionC.Value;
-        public Entity AddMoveDirection(ReactiveVariable<Vector3> direction) 
-            => AddComponent(new MoveDirection(){Value = direction});
-        public Entity AddMoveDirection() 
-            => AddComponent(new MoveDirection(){Value = new ReactiveVariable<Vector3>()});
+        // public MoveDirection MoveDirectionC => GetComponent<MoveDirection>();
+        // public ReactiveVariable<Vector3> MoveDirection => MoveDirectionC.Value;
+        // public Entity AddMoveDirection(ReactiveVariable<Vector3> direction) 
+        //     => AddComponent(new MoveDirection(){Value = direction});
+        // public Entity AddMoveDirection() 
+        //     => AddComponent(new MoveDirection(){Value = new ReactiveVariable<Vector3>()});
 
-        public MoveSpeed MoveSpeedC => GetComponent<MoveSpeed>();
-        public ReactiveVariable<float> MoveSpeed => MoveSpeedC.Value;
-        public Entity AddMoveSpeed(ReactiveVariable<float> speed) 
-            => AddComponent(new MoveSpeed(){Value = speed});
-        public Entity AddMoveSpeed() 
-            => AddComponent(new MoveSpeed(){Value = new ReactiveVariable<float>()});
+        // public MoveSpeed MoveSpeedC => GetComponent<MoveSpeed>();
+        // public ReactiveVariable<float> MoveSpeed => MoveSpeedC.Value;
+        // public Entity AddMoveSpeed(ReactiveVariable<float> speed) 
+        //     => AddComponent(new MoveSpeed(){Value = speed});
+        // public Entity AddMoveSpeed() 
+        //     => AddComponent(new MoveSpeed(){Value = new ReactiveVariable<float>()});
     }
 }
